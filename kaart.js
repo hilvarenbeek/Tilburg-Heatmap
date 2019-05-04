1cb15ee90222fa1972784b559d5812a1ce85541d
Marco van den Hout <marco@marco.vandenhout.org>
2019-05-04 23:47:40 +0200

    Cirkels voor vochtigheid, legenda voor hitte

--- kaart.js
+++ kaart.js
@@ -4,8 +4,8 @@ var meetjestadIds = ["251", "403", "410", "424", "427", "430", "437", "486", "49
 var meetWaarden = [];
 var minTemp, maxTemp;
 
-function Kaart() {
-    Initmap();
+function kaart() {
+    initmap();
 
     let url = 'https://meetjestad.net/data/sensors_json.php';
 
@@ -14,13 +14,14 @@ function Kaart() {
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             let jsonData = xhr.responseText;
-            Datalaag(jsonData);
+            datalaag(jsonData);
+            addLegend();
         }
     }
     xhr.send();
 }
 
-function Initmap() {
+function initmap() {
     let osm = new L.StamenTileLayer("toner-lite");
     map = new L.Map('map', { center: new L.LatLng(51.5554, 5.0824), zoom: 13 });
     map.addLayer(osm);
@@ -36,16 +37,38 @@ function Initmap() {
     map.attributionControl.addAttribution('Data points from <a href="http://meetjestad.net/">Meet je Stad</a>');
 }
 
-function Datalaag(jsonData) {
+function addLegend() {
+    let legend = L.control({ position: 'bottomleft' });
+    legend.onAdd = function(map) {
+        let div = L.DomUtil.create('div', 'info legend');
+        // verschil tussen laagste en hoogste temperatuur
+        let range = (Math.round(maxTemp * 100) - Math.round(minTemp * 100)) / 100;
+        let step = range / 10;
+
+        for (var i = 0; i < 10; i++) {
+            div.innerHTML +=
+                '<i style="background:' + calcHeatColor(i, 0, 10) + '"></i>' +
+                (Math.round((minTemp + (i * step)) * 100)) / 100 +
+                '<br/>';
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
+}
+
+function datalaag(jsonData) {
     let metingen = JSON.parse(jsonData);
     metingen.features.forEach(meting => {
 
         // alleen meetellen als id in het lijstje van Tilburg staat
         if (meetjestadIds.includes(meting.properties.id)) {
             let temp = parseFloat(meting.properties.temperature);
+            let humidity = parseFloat(meting.properties.humidity);
             if ((!minTemp) || (temp < minTemp)) { minTemp = temp };
             if ((!maxTemp) || (temp > maxTemp)) { maxTemp = temp };
-            meetWaarden.push({ lat: meting.geometry.coordinates[1], long: meting.geometry.coordinates[0], temp: temp });
+            meetWaarden.push({ lat: meting.geometry.coordinates[1], long: meting.geometry.coordinates[0], temp: temp, humidity: humidity });
         }
     })
 
@@ -57,6 +80,19 @@ function redrawD3Layer() {
 }
 
 function drawD3Layer(map, meetWaarden) {
+    d3.select("#map")
+        .select("svg")
+        .selectAll("myCircles")
+        .data(meetWaarden)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return map.latLngToLayerPoint([d.lat, d.long]).x })
+        .attr("cy", function(d) { return map.latLngToLayerPoint([d.lat, d.long]).y })
+        .attr("r", 40)
+        .style("fill", function(d) { return calcHumidityColor(d.humidity) })
+        .attr("stroke", function(d) { return calcHumidityColor(d.humidity) })
+        .attr("stroke-width", 1)
+        .attr("fill-opacity", .6);
     d3.select("#map")
         .select("svg")
         .selectAll("myCircles")
@@ -83,9 +119,16 @@ function drawD3Layer(map, meetWaarden) {
 function calcHeatColor(temp, min, max) {
     // verschil tussen laagste en hoogste temperatuur
     let range = Math.round(max * 100) - Math.round(min * 100);
-    // kleur is in HSL, H waarde 0=rood, 250=diepblauw
-    var h = 250 - (Math.round(temp * 100) - Math.round(min * 100)) * (250 / range);
+    // kleur is in HSL, H waarde 0=rood, 125=groen, 250=diepblauw
+    let h = 125 - (Math.round(temp * 100) - Math.round(min * 100)) * (125 / range);
     // S=verzadiging (grijs tot pure kleur), L=Lightness (zwart via pure kleur naar wit)
-    var hsl = { h: h, s: 100, l: 50 };
+    let hsl = { h: h, s: 100, l: 50 };
+    return Color().fromHsl(hsl).toString();
+}
+
+function calcHumidityColor(humidity) {
+    if (humidity > 100) { humidity = 100 };
+    let sat = Math.floor(humidity);
+    let hsl = { h: 240, s: sat, l: 50 };
     return Color().fromHsl(hsl).toString();
 }
